856e0eb2c509d95211d9407aca4d364fa563980c
jamieoglethorpe@gmail.com <jamieoglethorpe@gmail.com@0e2a6c64-82df-424a-a962-0763e2f10e7b>
2013-03-02 07:09:58 +0000

    Add GenLibrary user control

---
+++ GenEdit/View/GenLibrary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GenEdit.View
+{
+    public partial class GenLibrary : UserControl
+    {
+        public GenLibrary()
+        {
+            InitializeComponent();
+        }
+    }
+}
