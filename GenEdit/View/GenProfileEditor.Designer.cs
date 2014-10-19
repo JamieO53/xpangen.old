a1e0593d760b4c6bf740b888fc13c4929538f820
jamieoglethorpe <29419994+JamieO53@users.noreply.github.com>
2014-10-19 08:55:41 +0000

    Use rich text boxes for profile editor

--- GenEdit/View/GenProfileEditor.Designer.cs
+++ GenEdit/View/GenProfileEditor.Designer.cs
@@ -35,6 +35,15 @@
             this.ProfileNavigatorTreeView = new System.Windows.Forms.TreeView();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ProfileNavigatorImageList = new System.Windows.Forms.ImageList(this.components);
+            this.ProfileTextBox = new System.Windows.Forms.RichTextBox();
             this.textUserControl = new GenEdit.UserControls.TextUserControl();
             this.placeholderUserControl = new GenEdit.UserControls.PlaceholderUserControl();
             this.profileUserControl = new GenEdit.UserControls.ProfileUserControl();
@@ -45,16 +54,7 @@
             this.functionUserControl = new GenEdit.UserControls.FunctionUserControl();
             this.textBlockUserControl = new GenEdit.UserControls.TextBlockUserControl();
             this.annotationUserControl = new GenEdit.UserControls.AnnotationUserControl();
-            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
-            this.ProfileTextBox = new System.Windows.Forms.TextBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.ProfileExpansionTextBox = new System.Windows.Forms.TextBox();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.ProfileNavigatorImageList = new System.Windows.Forms.ImageList(this.components);
+            this.ProfileExpansionTextBox = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -146,6 +146,109 @@
             this.label1.Size = new System.Drawing.Size(111, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Profile navigator";
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.ProfileTextBox);
+            this.splitContainer3.Panel1.Controls.Add(this.panel3);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.ProfileExpansionTextBox);
+            this.splitContainer3.Panel2.Controls.Add(this.panel4);
+            this.splitContainer3.Size = new System.Drawing.Size(443, 472);
+            this.splitContainer3.SplitterDistance = 224;
+            this.splitContainer3.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(443, 20);
+            this.panel3.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Profile text";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(443, 20);
+            this.panel4.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Generated profile text";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(223, 20);
+            this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Fragment definition";
+            // 
+            // ProfileNavigatorImageList
+            // 
+            this.ProfileNavigatorImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ProfileNavigatorImageList.ImageStream")));
+            this.ProfileNavigatorImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.ProfileNavigatorImageList.Images.SetKeyName(0, "DataBrowserSelection.png");
+            this.ProfileNavigatorImageList.Images.SetKeyName(1, "ProfileBrowserText.png");
+            this.ProfileNavigatorImageList.Images.SetKeyName(2, "ProfileBrowserPlaceholder.png");
+            this.ProfileNavigatorImageList.Images.SetKeyName(3, "ProfileBrowserSegment.png");
+            this.ProfileNavigatorImageList.Images.SetKeyName(4, "ProfileBrowserSegment.png");
+            this.ProfileNavigatorImageList.Images.SetKeyName(5, "ProfileBrowserSegment.png");
+            this.ProfileNavigatorImageList.Images.SetKeyName(6, "ProfileBrowserLookup.png");
+            this.ProfileNavigatorImageList.Images.SetKeyName(7, "ProfileBrowserCondition.png");
+            this.ProfileNavigatorImageList.Images.SetKeyName(8, "ProfileBrowserFunction.png");
+            // 
+            // ProfileTextBox
+            // 
+            this.ProfileTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProfileTextBox.Location = new System.Drawing.Point(0, 20);
+            this.ProfileTextBox.Name = "ProfileTextBox";
+            this.ProfileTextBox.Size = new System.Drawing.Size(443, 204);
+            this.ProfileTextBox.TabIndex = 2;
+            this.ProfileTextBox.Text = "";
             // 
             // textUserControl
             // 
@@ -248,119 +351,14 @@
             this.annotationUserControl.TabIndex = 2;
             this.annotationUserControl.Visible = false;
             // 
-            // splitContainer3
-            // 
-            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer3.Name = "splitContainer3";
-            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer3.Panel1
-            // 
-            this.splitContainer3.Panel1.Controls.Add(this.ProfileTextBox);
-            this.splitContainer3.Panel1.Controls.Add(this.panel3);
-            // 
-            // splitContainer3.Panel2
-            // 
-            this.splitContainer3.Panel2.Controls.Add(this.ProfileExpansionTextBox);
-            this.splitContainer3.Panel2.Controls.Add(this.panel4);
-            this.splitContainer3.Size = new System.Drawing.Size(443, 472);
-            this.splitContainer3.SplitterDistance = 224;
-            this.splitContainer3.TabIndex = 1;
-            // 
-            // ProfileTextBox
-            // 
-            this.ProfileTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ProfileTextBox.Location = new System.Drawing.Point(0, 20);
-            this.ProfileTextBox.Multiline = true;
-            this.ProfileTextBox.Name = "ProfileTextBox";
-            this.ProfileTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.ProfileTextBox.Size = new System.Drawing.Size(443, 204);
-            this.ProfileTextBox.TabIndex = 2;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.label3);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(443, 20);
-            this.panel3.TabIndex = 1;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(0, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 17);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Profile text";
-            // 
             // ProfileExpansionTextBox
             // 
             this.ProfileExpansionTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ProfileExpansionTextBox.Location = new System.Drawing.Point(0, 20);
-            this.ProfileExpansionTextBox.Multiline = true;
             this.ProfileExpansionTextBox.Name = "ProfileExpansionTextBox";
-            this.ProfileExpansionTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.ProfileExpansionTextBox.Size = new System.Drawing.Size(443, 224);
-            this.ProfileExpansionTextBox.TabIndex = 3;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.label4);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(0, 0);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(443, 20);
-            this.panel4.TabIndex = 1;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(0, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(145, 17);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Generated profile text";
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(223, 20);
-            this.panel2.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(0, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 17);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Fragment definition";
-            // 
-            // ProfileNavigatorImageList
-            // 
-            this.ProfileNavigatorImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ProfileNavigatorImageList.ImageStream")));
-            this.ProfileNavigatorImageList.TransparentColor = System.Drawing.Color.Transparent;
-            this.ProfileNavigatorImageList.Images.SetKeyName(0, "DataBrowserSelection.png");
-            this.ProfileNavigatorImageList.Images.SetKeyName(1, "ProfileBrowserText.png");
-            this.ProfileNavigatorImageList.Images.SetKeyName(2, "ProfileBrowserPlaceholder.png");
-            this.ProfileNavigatorImageList.Images.SetKeyName(3, "ProfileBrowserSegment.png");
-            this.ProfileNavigatorImageList.Images.SetKeyName(4, "ProfileBrowserSegment.png");
-            this.ProfileNavigatorImageList.Images.SetKeyName(5, "ProfileBrowserSegment.png");
-            this.ProfileNavigatorImageList.Images.SetKeyName(6, "ProfileBrowserLookup.png");
-            this.ProfileNavigatorImageList.Images.SetKeyName(7, "ProfileBrowserCondition.png");
-            this.ProfileNavigatorImageList.Images.SetKeyName(8, "ProfileBrowserFunction.png");
+            this.ProfileExpansionTextBox.TabIndex = 2;
+            this.ProfileExpansionTextBox.Text = "";
             // 
             // GenProfileEditor
             // 
@@ -381,9 +379,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.splitContainer3.Panel1.ResumeLayout(false);
-            this.splitContainer3.Panel1.PerformLayout();
             this.splitContainer3.Panel2.ResumeLayout(false);
-            this.splitContainer3.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
             this.splitContainer3.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
@@ -406,10 +402,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox ProfileTextBox;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox ProfileExpansionTextBox;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ImageList ProfileNavigatorImageList;
@@ -423,5 +417,7 @@
         private UserControls.FunctionUserControl functionUserControl;
         private UserControls.TextBlockUserControl textBlockUserControl;
         private UserControls.AnnotationUserControl annotationUserControl;
+        private System.Windows.Forms.RichTextBox ProfileTextBox;
+        private System.Windows.Forms.RichTextBox ProfileExpansionTextBox;
     }
 }
