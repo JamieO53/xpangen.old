bd511e33448991f4f6066af17484504fe5b17f5c
jamieo <jamieo@0e2a6c64-82df-424a-a962-0763e2f10e7b>
2015-04-20 05:54:48 +0000

    Refactoring towards parallel loading of data and profile - adding classes to repository

---
+++ gen/Loader.cs
@@ -0,0 +1,73 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using System.IO;
+using org.xpangen.Generator.Data;
+using org.xpangen.Generator.Parameter;
+using org.xpangen.Generator.Profile;
+using org.xpangen.Generator.Profile.Parser.CompactProfileParser;
+
+namespace gen
+{
+    public class Loader
+    {
+        public GenParameters GenParameters { get; set; }
+        public GenDataDef GenDataDef { get; set; }
+        public GenProfileFragment GenProfileFragment { get; set; }
+        public static Loader Load(Args args)
+        {
+            GenDataLoader.Register();
+            var loader = new Loader();
+            loader.LoadFiles(args);
+            return loader;
+        }
+
+        private Args Args { get; set; }
+        private void LoadFiles(Args args)
+        {
+            Args = args;
+            if (Args.Definition != "")
+            {
+                LoadDef(args);
+                LoadParametersWithDef();
+                LoadProfile();
+            }
+            else
+            {
+                LoadParametersWithoutDef();
+                LoadProfile();
+            }
+            GenProfileFragment.GenObject = GenParameters.Root;
+        }
+
+        private void LoadParametersWithoutDef()
+        {
+            using (var dataStream = new FileStream(Args.Data, FileMode.Open))
+                GenParameters = new GenParameters(dataStream) {DataName = Path.GetFileNameWithoutExtension(Args.Data)};
+            GenDataDef = GenParameters.GenDataDef;
+        }
+
+        private void LoadProfile()
+        {
+            GenProfileFragment = new GenCompactProfileParser(GenDataDef, Args.Profile, "", Args.Delimiter[0]);
+        }
+
+        private void LoadDef(Args args)
+        {
+            GenParameters f;
+            using (var definitionStream = new FileStream(args.Definition, FileMode.Open))
+                f = new GenParameters(definitionStream) {DataName = Path.GetFileNameWithoutExtension(args.Definition)};
+            GenDataDef = f.AsDef();
+        }
+
+        private void LoadParametersWithDef()
+        {
+            using (var dataStream = new FileStream(Args.Data, FileMode.Open))
+                GenParameters = new GenParameters(GenDataDef, dataStream)
+                {
+                    DataName = Path.GetFileNameWithoutExtension(Args.Data)
+                };
+        }
+    }
+}
