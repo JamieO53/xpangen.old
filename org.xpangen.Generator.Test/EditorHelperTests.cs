42d3b6b6b0345afac3c2a5e3e3ef1f26f4101b47
jamieo <jamieo@0e2a6c64-82df-424a-a962-0763e2f10e7b>
2015-05-01 14:50:05 +0000

    Clean up

--- org.xpangen.Generator.Test/EditorHelperTests.cs
+++ org.xpangen.Generator.Test/EditorHelperTests.cs
@@ -1,8 +1,6 @@
 ﻿using System.IO;
-using org.xpangen.Generator.Data;
-using org.xpangen.Generator.Editor.Helper;
 using NUnit.Framework;
-using org.xpangen.Generator.Data.Model.Settings;
+using org.xpangen.Generator.Editor.Helper;
 using org.xpangen.Generator.Parameter;
 
 namespace org.xpangen.Generator.Test
